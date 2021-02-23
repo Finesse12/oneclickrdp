e6b78b61b3624d89c80c81131f5d9a1ed142b66d
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-23 17:31:07 +0530

    Update oneclickrdp.sh

--- oneclickrdp.sh
+++ oneclickrdp.sh
@@ -2,10 +2,6 @@ sudo apt update
 sudo apt -y install screen
 sudo apt -y install xfce4
 sudo apt -y install firefox
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt -y install anydesk
 sudo apt install tightvncserver
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
